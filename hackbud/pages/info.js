28875e321294f4a0ed5f8c10de919f556a755577
Utkarsh <utkarshnagar2000@gmail.com>
2023-05-30 22:16:20 +0530

    form

--- hackbud/pages/info.js
+++ hackbud/pages/info.js
@@ -1,24 +1,31 @@
-import api from "@/components/appwrite";
-import { useEffect } from "react";
+import api from '@/components/appwrite'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-function Info(){
-    const {account,databases}  = api();
+function Info() {
+    const router = useRouter()
+    const { account, listD, getSession } = api()
+    const [postInfoTeam, setPostInfoTeam] = useState(false)
+    const [postInfoTeammate, setPostInfoTeammate] = useState(false)
 
-    useEffect(async ()=>{
-        const accountInfo = await account.get()
-        console.log(accountInfo)
-    })
+    useEffect(() => {}, [])
 
-    const postInfo = () => {
-        
-    }
+    const handlePostInfoTeam = () => {}
 
-    return(
+    const handlePostInfoTeammate = () => {}
+
+    return (
         <div>
             <h1>Info</h1>
+            <Link href="/form">
+                Looking for a teammate ?
+            </Link>
+            <Link href="/form">
+                Want to be part of a team ?
+            </Link>
         </div>
-    );
+    )
 }
 
-
-export default Info;
+export default Info
