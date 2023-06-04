2794a2e2f386b3b5a98aa5799ad2d7a8763c88e1
Utkarsh <utkarshnagar2000@gmail.com>
2023-06-04 16:52:47 +0530

    Layout, navbar, footer and teammates page added

---
+++ hackbud/components/Footer.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+export default function Footer() {
+    return (
+      <div className="w-full bg-black px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center text-center text-white">
+          <span className="font-semibold">© 2023 </span>
+          <Link
+            href="https://github.com/Utkarshn10"
+            className="ml-1 font-semibold hover:underline underline-offset-2"
+          >
+            Utkarsh Nagar
+          </Link>
+          <span className="ml-1 font-semibold">· All rights reserved.</span>
+        </div>
+      </div>
+    );
+  }
