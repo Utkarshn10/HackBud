cd7816bb7fdc9378ed339101e10520464f0f8871
Utkarsh <utkarshnagar2000@gmail.com>
2023-06-07 23:18:57 +0530

    session

--- hackbud/pages/index.js
+++ hackbud/pages/index.js
@@ -5,14 +5,14 @@ import { useEffect } from 'react'
 
 export default function Home() {
     const { account } = api()
-    const promise = account.getSession('current')
+    const promise = account.getOauth2Session('current')
 
     promise.then(
         function (response) {
             console.log(response) // Success
         },
         function (error) {
-            console.log(error) // Failure
+            console.log("error = ",error) // Failure
         }
     )
     return (
