3fd1642526cc71ff7bfafec7b655317adb92e3b5
Utkarsh <utkarshnagar2000@gmail.com>
2023-06-11 20:15:25 +0530

    choices page improvements

--- hackbud/pages/choices.js
+++ hackbud/pages/choices.js
@@ -1,12 +1,32 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import api from '@/components/appwrite'
 
 export default function Choices() {
+    const { account} = api()
+    const router = useRouter()
+
+
     function logoutClicked() {
         console.log('Session deleted')
         deleteCurrentSession()
         router.push('/')
     }
+
+    useEffect(()=>{
+        const promise = account.get()
+        promise.then(
+            function (response) {
+            },
+            function (error) {
+                console.log('error = ', error) // Failure
+                router.push('/')
+                return false
+            }
+        )
+    },[])
     return (
         <div className="w-full">
             <nav className="flex bg-[#141D2C] justify-between">
@@ -27,29 +47,28 @@ export default function Choices() {
                 </div>
             </nav>
 
-            <div className="flex items-center justify-center bg-white w-full min-h-screen py-2 flex-col">
-                <div className="flex items-center justify-center flex-col mt-4">
+            <div className="flex items-center justify-center bg-white w-full py-2 flex-col">
+                <div className="flex items-center justify-center flex-col  mt-4">
                     <h2 className="font-bold text-4xl text-black text-center mb-4">
                         Collaborate, Innovate, Succeed
                     </h2>
-                    <div className="text-center w-full md:w-3/5 px-4 md:px-6 font-medium text-sm md:text-lg mb-4">
+                    <div className="text-center w-full md:w-2/5 px-4 md:px-6 font-medium text-sm md:text-lg">
                         <div className="text-slate-400 font-normal">
                             Harness the power of collaboration. Connect with
                             driven individuals who are eager to work together,
                             share ideas, and push the boundaries of innovation.
-                            Achieve your goals and create a legacy.
                         </div>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-5xl px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-center">
                         <div className="bg-white w-3/4 text-black border-2 border-slate-200 rounded-3xl my-2 md:my-10 cursor-pointer flex flex-col items-center justify-center hover:shadow-xl hover:border-slate-300">
                             <Link
                                 href="/team-form"
-                                className="w-full h-50"
+                                className="w-full h-30"
                                 style={{
-                                    maxWidth: '100%',
-                                    maxHeight: '100%',
+                                    maxWidth: '70%',
+                                    maxHeight: '40%',
                                     overflow: 'hidden',
                                 }}
                             >
@@ -71,10 +90,10 @@ export default function Choices() {
                     <div className="flex items-center justify-center">
                         <div className="bg-white w-3/4 text-black border-2 border-slate-200 rounded-3xl my-2 md:my-10 cursor-pointer flex flex-col items-center justify-center hover:shadow-xl hover:border-slate-300">
                             <Link
-                                className="w-full h-50"
+                                className="w-full h-30"
                                 style={{
-                                    maxWidth: '100%',
-                                    maxHeight: '100%',
+                                    maxWidth: '70%',
+                                    maxHeight: '40%',
                                     overflow: 'hidden',
                                 }}
                                 href="need-teammate-form"
