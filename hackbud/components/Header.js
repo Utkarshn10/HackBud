51b9b74c96f33e201fc878c8c576658168824989
Utkarsh <utkarshnagar2000@gmail.com>
2023-07-27 17:14:28 +0530

    text aligned

--- hackbud/components/Header.js
+++ hackbud/components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <div className='mt-12'>
+        <div className="mt-12">
             {' '}
             <div className="flex justify-center items-center text-center px-4">
                 <div className="text-3xl md:text-8xl font-bold text-white">
@@ -13,15 +13,12 @@ export default function Header() {
                 </div>
             </div>
             <div className="flex justify-center items-center px-10 md:px-0 text-sm md:text-xl md:leading-8 my-6 flex-col text-slate-400">
-                Are you ready to take your hackathon experience to the next
-                level? Meet HackBud,{' '}
-                <p className="flex justify-center ">
-                    the ultimate team/teammate finder web app. Connect with
-                    like-minded individuals, build{' '}
-                </p>{' '}
-                <p className="flex justify-center ">
-                    dream teams, and make your hackathon dreams come true. Sign
-                    up for free now!
+                <p className="flex justify-center text-center w-2/3">
+                    Are you ready to take your hackathon experience to the next
+                    level? Meet HackBud, the ultimate team/teammate finder web
+                    app. Connect with like-minded individuals, build dream
+                    teams, and make your hackathon dreams come true. Sign up for
+                    free now!
                 </p>
                 <Link
                     href="/signup"
