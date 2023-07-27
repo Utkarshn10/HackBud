761dd871ad8765132c9864bf94c28d0419f478aa
Utkarsh <utkarshnagar2000@gmail.com>
2023-07-27 15:59:10 +0530

    landing page

---
+++ hackbud/components/Faq.js
@@ -0,0 +1,49 @@
+function FaqCard({ question, answer }) {
+    return (
+        <div className="bg-slate-800 m-4 p-8 rounded-xl">
+            <h3 className="text-white font-normal text-base md:text-xl">{question}</h3>
+            <div className="text-slate-400 text-sm md:text-lg mt-4">{answer}</div>
+        </div>
+    )
+}
+
+export default function Faq() {
+    const data = [
+        [
+            'Can I use HackBud to find teammates for specific technology stacks or project domains?',
+            "Absolutely! HackBud's Team Finder and Teammate Finder pages allow you to filter and search based on specific criteria such as project domain, required skills, and technologies involved. This makes it easier for you to find teammates who align with your project requirements",
+        ],
+        [
+            'Can I create a profile for my existing team on HackBud?',
+            "Yes, you can create a profile for your existing team on HackBud. Provide detailed information about your team's skills, expertise, and areas of interest to attract potential teammates who align with your goals",
+        ],
+        [
+            'How can I connect with potential team leads or teammates on HackBud?',
+            'Yes, you can find the necessary contact details of a team or team lead on their profile page. This typically includes their email address and potentially other ways to connect, such as a LinkedIn profile or a personal website',
+        ],
+        [
+            'Can I use HackBud to participate in hackathons globally?',
+            "HackBud allows you to participate in hackathons globally, connecting you with hackathon enthusiasts worldwide. Whether you're seeking teammates for a local event or looking to join a global hackathon, HackBud helps you find the right people to form winning teams",
+        ],
+        ['Is HackBud free to use?', 'Yes, HackBud is completely free to use'],
+    ]
+    return (
+        <>
+            <div className="flex justify-center text-center mt-4 md:mt-8">
+                <div className="flex flex-col">
+                    <div className="text-white font-bold text-2xl md:text-5xl">
+                        Frequently asked questions
+                    </div>
+                    <div className="text-slate-400 text-lg md:text-xl my-4 md:my-8 flex justify-center ">
+                        We have put together some commonly asked questions
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                {data.map((item) => (
+                    <FaqCard question={item[0]} answer={item[1]} />
+                ))}
+            </div>
+        </>
+    )
+}
--- hackbud/pages/index.js
+++ hackbud/pages/index.js
@@ -4,6 +4,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { BsArrowRightCircle } from 'react-icons/bs'
 import Link from 'next/link'
+import Faq from '@/components/Faq'
+import Header from '@/components/Header'
 
 export default function Home() {
     const { account } = api()
@@ -15,53 +17,8 @@ export default function Home() {
             </Head>
             <div className="w-full">
                 <Navbar />
-                
-
-                <div className="lg:overflow-hidden flex flex-col md:flex-row mx-auto mb-auto items-center justify-center md:py-16">
-                    <div className="max-w-7xl font-Montserrat:wght@300 text-center sm:text-left mx-2">
-                        <h1 className="relative flex flex-col px-4 items-center mt-8 md:mt-4 ">
-                            <h1 className="text-4xl md:text-7xl my-2 md:my-4">
-                                <span className="inline-block font-extrabold tracking-tight bg-gradient-to-r from-[#F9D307] to-[#FCA50A] bg-clip-text text-transparent">
-                                Unleash 
-                                </span>{' '}
-                                <br />
-                                <span className="inline-block font-extrabold tracking-tight bg-gradient-to-r from-[#FCA50A] to-[#FF8205] bg-clip-text text-transparent">
-                                Your 
-                                </span>{' '}
-                                <span className="inline-block font-extrabold tracking-tight bg-gradient-to-r from-[#FCA50A] to-[#FF8205] bg-clip-text text-transparent">
-                                Hackathon 
-                                </span>{' '}
-                                <br />
-                                <span className="inline-block font-extrabold tracking-tight text-[#F9D307]">
-                                Potential
-                                </span>
-                                <p className="text-base lg:text-2xl font-bold mt-4 md:mt-6  text-gray-300 ">
-                                Empowering Collaboration in Hackathons
-                            </p>
-                            <div className='flex items-center justify-center md:justify-start'>
-                            <Link
-                            href="/signup"
-                            className="flex mt-8 w-1/2 mb-4 md:mb-0 items-center rounded-xl p-4 justify-center font-lato text-[#332A36] bg-white text-sm md:text-lg font-medium"
-                        >
-                            Get Started
-                            <div className="pl-3">
-                                <BsArrowRightCircle />
-                            </div>
-                        </Link>
-                        </div>
-                            </h1>
-                          
-                        </h1>
-                    </div>
-                    <div className="mx-5 md:my-0 md:mr-7 md:mx-0">
-                        <Image
-                            src="/home-bg.png"
-                            height={1000}
-                            width={800}
-                            className="rounded-3xl mx-auto shadow-xl"
-                        />
-                    </div>
-                </div>
+                <Header />
+                <Faq />
             </div>
         </>
     )
