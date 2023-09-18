0ce2407498bfc47ef236c39096fc259dd4c08e34
Utkarsh <utkarshnagar2000@gmail.com>
2023-09-18 16:21:03 +0530

    meta tags added

--- hackbud/pages/index.js
+++ hackbud/pages/index.js
@@ -3,12 +3,30 @@ import Navbar from '@/components/Navbar'
 import Head from 'next/head'
 import Faq from '@/components/Faq'
 import Header from '@/components/Header'
+import Head from 'next/head';
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>HackBud</title>
+                <Head>
+                    <title>Hackbud - Hackathon Team Building App</title>
+                    <meta
+                        name="description"
+                        content="Connect with like-minded individuals and build teams for hackathons on Hackbud, the ultimate team building app."
+                    />
+
+                    {/* Set other meta tags as needed */}
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                    <meta
+                        name="keywords"
+                        content="hackathon, team building, app, Hackbud"
+                    />
+                    <meta name="author" content="Utkarsh Nagar" />
+                </Head>
             </Head>
             <div className="w-full">
                 <Header />
