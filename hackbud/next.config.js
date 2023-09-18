b2dcf9932c0c682d8b78f014dacfdc8cd01ad115
Utkarsh <utkarshnagar2000@gmail.com>
2023-09-18 16:38:11 +0530

    fix

--- hackbud/next.config.js
+++ hackbud/next.config.js
@@ -2,20 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = {
-  // Other Next.js configurations
-  // ...
 
-  // Serve files from the public folder
-  // This should be the default behavior, but it's good to check
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
-
-    return config;
-  },
-};
-
+module.exports = nextConfig
