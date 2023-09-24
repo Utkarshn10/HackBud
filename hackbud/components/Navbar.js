5981dd6579c425da9096c7f8fbdc6a095422398a
Utkarsh <utkarshnagar2000@gmail.com>
2023-09-24 16:19:09 +0530

    mobile navbar fixed

--- hackbud/components/Navbar.js
+++ hackbud/components/Navbar.js
@@ -238,21 +238,7 @@ export default function Navbar() {
                                         Logout
                                     </button>
                                 </div>
-                            ) : (
-                                <div
-                                    ref={ref}
-                                    className="px-2 pt-2 pb-3 space-y-1 sm:px-3"
-                                >
-                                    <Link
-                                        href="/login"
-                                        className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                                    >
-                                        Login with GitHub
-                                    </Link>
-
-                                    
-                                </div>
-                            )}
+                            ) : null}
                         </div>
                     )}
                 </Transition>
